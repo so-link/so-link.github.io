4e8233cf8e5f24fe5b3d0f6b4c3e547865105b89
SCUTliu22 <78074710+SCUTliu22@users.noreply.github.com>
2025-07-02 12:12:56 +0000

    Deploying to gh-pages from @ so-link/so-link.github.io@5a95cbcb61b2bcc210a840c9c72dfa7552081f5f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-实验室简介",
-    title: "实验室简介",
+    id: "nav-简介",
+    title: "简介",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
