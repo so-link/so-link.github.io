19737fec30e4da056e76894cdc4abd6ab9fcac28
SCUTliu22 <78074710+SCUTliu22@users.noreply.github.com>
2025-09-10 13:33:39 +0000

    Deploying to gh-pages from @ so-link/so-link.github.io@639727bfe97717ce6f1892f93698ffd791f6f0f2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -109,6 +109,11 @@ ninja.data = [{
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
+              window.location.href = "/projects/JieYuchan/";
+            },},{id: "projects-吕建明",
+          title: '吕建明',
+          description: "实验室导师，华南理工大学计算机科学与工程学院教授，华南理工大学科学与工程学院副院长",
+          section: "Projects",handler: () => {
               window.location.href = "/projects/LvJianming/";
             },},{
         id: 'social-email',
