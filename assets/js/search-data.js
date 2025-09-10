532e8d23300759bb7ff6cafeac079e73e7c1df83
SCUTliu22 <78074710+SCUTliu22@users.noreply.github.com>
2025-09-10 13:19:19 +0000

    Deploying to gh-pages from @ so-link/so-link.github.io@101cb8bd84dd3a362fba97d442406a2ca1712bb0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -105,6 +105,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-project-1",
+          title: 'project 1',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/LvJianming/";
             },},{
         id: 'social-email',
         title: 'email',
