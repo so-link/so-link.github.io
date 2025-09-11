cd3a1001075d668f3fb6426eb9f84eaecc1f3af4
SCUTliu22 <78074710+SCUTliu22@users.noreply.github.com>
2025-09-11 13:54:51 +0000

    Deploying to gh-pages from @ so-link/so-link.github.io@755b9b6b8af2f718319ef8ce82335f1a46d84e35 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -53,58 +53,28 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-多模态儿童医疗",
+          title: '多模态儿童医疗',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
+            },},{id: "projects-华为5g信号处理",
+          title: '华为5G信号处理',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
+            },},{id: "projects-重定向测试",
+          title: '重定向测试',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{id: "projects-接玉婵",
           title: '接玉婵',
           description: "24级在读博士",
           section: "Projects",handler: () => {
               window.location.href = "/projects/JieYuchan/";
-            },},{id: "projects-接玉婵",
-          title: '接玉婵',
+            },},{id: "projects-刘尚霖",
+          title: '刘尚霖',
           description: "25级在读博士",
           section: "Projects",handler: () => {
               window.location.href = "/projects/LiuShanglin/";
