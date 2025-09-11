06700c46bc672e179f6062c93c43d5a69ec9e0eb
SCUTliu22 <78074710+SCUTliu22@users.noreply.github.com>
2025-09-11 13:13:35 +0000

    Deploying to gh-pages from @ so-link/so-link.github.io@c59429772679d6840faf49479dbcb5f5c3630fc0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,7 +28,7 @@ ninja.data = [{
           description: "已毕业或仍在实验室工作的成员",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/projects/";
+            window.location.href = "/project/";
           },
         },{id: "nav-课程",
           title: "课程",
